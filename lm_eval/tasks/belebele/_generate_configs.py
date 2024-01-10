2380ebc6e8602ada892a2dc48b938dd2b40aa6b1
Julen Etxaniz <juletxara@gmail.com>
2024-01-10 11:00:44 +0100

    fix belebele task to match huggingface dataset format

--- lm_eval/tasks/belebele/_generate_configs.py
+++ lm_eval/tasks/belebele/_generate_configs.py
@@ -8,10 +8,10 @@ import requests
 
 from tqdm import tqdm
 
-from lm_eval.logger import eval_logger
 
 API_URL = "https://datasets-server.huggingface.co/splits?dataset=facebook/belebele"
 
+# python _generate_configs.py --base_yaml_path _default_template_yaml --save_prefix_path belebele
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -40,7 +40,7 @@ if __name__ == "__main__":
         response = requests.get(API_URL)
         return response.json()["splits"]
 
-    languages = [split["config"] for split in query()]
+    languages = [split["split"] for split in query()]
 
     for lang in tqdm(languages):
         yaml_dict = {
@@ -48,11 +48,11 @@ if __name__ == "__main__":
             "task": f"belebele_{args.task_prefix}_{lang}"
             if args.task_prefix != ""
             else f"belebele_{lang}",
-            "dataset_name": lang,
+            "test_split": lang,
+            "fewshot_split": lang,
         }
 
         file_save_path = args.save_prefix_path + f"_{lang}.yaml"
-        eval_logger.info(f"Saving yaml for subset {lang} to {file_save_path}")
         with open(file_save_path, "w") as yaml_file:
             yaml.dump(
                 yaml_dict,
