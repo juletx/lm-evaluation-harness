b7d168e9ee16266a122d4691947e134058bd0953
Julen Etxaniz <juletxara@gmail.com>
2024-01-28 21:44:50 +0100

    add all remaining euskaljakintza multiple-choice tasks

--- lm_eval/tasks/euskaljakintza/utils.py
+++ lm_eval/tasks/euskaljakintza/utils.py
@@ -1,5 +1,7 @@
 from typing import List
 
+letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+
 
 def doc_to_text(doc) -> str:
     """
@@ -13,9 +15,30 @@ def doc_to_text(doc) -> str:
     """
     candidates = doc["candidates"]
     num_choices = len(candidates)
-    if num_choices < 2 or num_choices > 4:
+    if num_choices < 2:
         raise ValueError("Invalid number of candidates")
-    choices = ["A", "B", "C", "D"][:num_choices]
+    choices = letters[:num_choices]
+    formatted_choices = "\n".join(
+        [f"{choice}: {candidates[i]}" for i, choice in enumerate(choices)]
+    )
+    return f"Galdera: {doc['question']}\n{formatted_choices}\nErantzuna:"
+
+
+def doc_to_text_context(doc) -> str:
+    """
+    Converts a document to a formatted string.
+
+    Args:
+        doc (dict): A dictionary containing the document information.
+
+    Returns:
+        str: A formatted string containing the question and answer choices.
+    """
+    candidates = doc["candidates"]
+    num_choices = len(candidates)
+    if num_choices < 2:
+        raise ValueError("Invalid number of candidates")
+    choices = letters[:num_choices]
     formatted_choices = "\n".join(
         [f"{choice}: {candidates[i]}" for i, choice in enumerate(choices)]
     )
@@ -33,6 +56,6 @@ def doc_to_choice(doc) -> List[str]:
         list: A list of strings containing the answer choices.
     """
     num_choices = len(doc["candidates"])
-    if num_choices < 2 or num_choices > 4:
+    if num_choices < 2:
         raise ValueError("Invalid number of candidates")
-    return ["A", "B", "C", "D"][:num_choices]
+    return letters[:num_choices]
